545ba006833a1a3244b21492347dfb6c776b2a7c
anne474w <101199118+anne474w@users.noreply.github.com>
2025-11-07 13:31:49 +0100

    Add constructor and Fight method to Monster

--- StarWarsCore Extended/StarWarsCore/Models/Monster.cs
+++ StarWarsCore Extended/StarWarsCore/Models/Monster.cs
@@ -2,5 +2,15 @@ namespace StarWarsCore.Models;
 
 public class Monster : Creature
 {
+    public Monster()
+    {
+        Weaknesses.Add("Angel Blade");
+        Weaknesses.Add("Colt");
+    }
     public List<string> Weaknesses =  new List<string>();
+
+    public void Fight(Monster attacker, Hunter defender)
+    {
+        base.Fight(attacker, defender, 1);
+    }
 }
