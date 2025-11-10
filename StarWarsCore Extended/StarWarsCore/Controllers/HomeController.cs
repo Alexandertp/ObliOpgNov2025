f8ffc52387305d37ddc563779b21bf1ff808b779
anne474w <101199118+anne474w@users.noreply.github.com>
2025-11-10 13:25:45 +0100

    deleted all of Finns bullshit

--- StarWarsCore Extended/StarWarsCore/Controllers/HomeController.cs
+++ StarWarsCore Extended/StarWarsCore/Controllers/HomeController.cs
@@ -46,33 +46,6 @@ namespace StarWarsCore.Controllers
                     myWarrior.DarkSide = newWarrior.DarkSide;
                     myWarrior.Deceased = false;
 
-                    
-                    // start up first knight in game log
-                    gameLog.FightEvents.AddRange(ListOutput(myWarrior.FightLog.FightEvents));
-
-                    // Set up some more Jedi objects
-                    Darth DarthV = new Darth();
-                    // start up second knight in game log
-                    gameLog.FightEvents.AddRange(ListOutput(DarthV.FightLog.FightEvents));
-
-                    Luke LukeS = new Luke();
-                    // start up third knight in game log
-                    gameLog.FightEvents.AddRange(ListOutput(LukeS.FightLog.FightEvents));
-
-                    List<JediKnight> myJedis = new List<JediKnight>();
-                    myJedis.Add(myWarrior);
-                    myJedis.Add(DarthV);
-                    myJedis.Add(LukeS);
-
-                    // Dark horses: Obi-Wan and The Emperor
-                    Obi_Wan ObiWan = new Obi_Wan();
-                    gameLog.FightEvents.AddRange(ListOutput(ObiWan.FightLog.FightEvents));
-
-                    EvilEmperor Palpatine = new EvilEmperor();
-                    gameLog.FightEvents.AddRange(ListOutput(Palpatine.FightLog.FightEvents));
-                    // clear a few lines of output for spacing the battle data from preliminary speech
-                    gameLog.FightEvents.Add("</br></br>");
-
                     List<Hunter>hunters = new List<Hunter>();
                     List<Monster>monsters = new List<Monster>();
                     Sam sam = new Sam();
@@ -134,197 +107,21 @@ namespace StarWarsCore.Controllers
                         gameLog.FightEvents.Add("</br>");
                         gameLog.FightEvents.Add(hunters[offer].Name + " is " + hunters[offer].CurrentDamageLevel.ToString());
                         gameLog.FightEvents.Add("</br>");
-                        if ((int)hunters[offer].CurrentDamageLevel <= 2 && hunters[offer].Name != "Castiel")
+
+                        foreach (Hunter hunter in hunters)
                         {
-                            castiel.SaveABrother(hunters[offer]);
-                            gameLog.FightEvents.Add(castiel.Name + " has just saved " + hunters[offer].Name + " from near death!" + "</br>");
+                            if (hunter.Name == "Castiel" && !hunter.isDead)
+                            {
+                                if ((int)hunters[offer].CurrentDamageLevel <= 2 && hunters[offer].Name != "Castiel")
+                                {
+                                    castiel.SaveABrother(hunters[offer]);
+                                    gameLog.FightEvents.Add(castiel.Name + " has just saved " + hunters[offer].Name + " from near death!" + "</br>");
+
+                                }
+                            }
                         }
                     }
-                    // Run battle scenario umpteen times, enough to kill one of our protagonists outright a few times over. And to bring in Obi-Wan.
-                    for (int i = 1; i < 25; i++)
-                    {
-                        // Pick a random Jedi Knight to kick into action courtesy of our Helper class                             
-                        // int randomInt = RandomGenerator.Rand.Next(0, myJedis.Count);
-                        // JediKnight warrior = myJedis[randomInt];
-                        // Let ol' Palpatine decide who's next, rather than calling up the same guy by chance
-                        JediKnight warrior = Palpatine.PickJediKnight(myJedis);
-
-                        // Empty the individual fight log before next batch recording of events
-                        warrior.FightLog.FightEvents.Clear();
-                        warrior.FightLog.FightEvents.Add("The Evil Emperor randomly points a bony finger at " + warrior.Name + " and says: 'do some nasty work for me - now!'");
-
-                        // Attack opponents, but only if they are on the opposing side, and only if you aren't dead yourself..yet.            
-                        switch (warrior.Name)
-                        {
-                            case "Darth Vader":
-                                if (!warrior.Deceased)
-                                {
-                                    warrior.AttackEnemy(warrior, LukeS);
-                                    if (LukeS.CurrentDamageLevel == JediKnight.DamageLevel.Hurting)
-                                    {
-                                        // a few words of desparate pleading from Obi-Wan, since his protegé Luke is decidedly getting nastified by the dark lord of the Sith
-                                        warrior.FightLog.FightEvents.Add("Luke hears a well-known voice from beyond the grave.. Obi-Wan says: " + ObiWan.Dismay);
-                                    }
-                                    // Attack the player if he's not a Sith
-                                    if (myWarrior.DarkSide == false) { warrior.AttackEnemy(warrior, myWarrior); }
-
-                                    // Attack Obi-Wan, if he is in the game
-                                    if (myJedis.Contains(ObiWan)) { warrior.AttackEnemy(warrior, ObiWan); }
-
-                                    // if Darth Vader is pressured with a health level of critical..bring his boss' secret weapon to bear
-                                    // if ((warrior.currentDamageLevel == JediKnight.DamageLevel.Critical) || warrior.NumOfDeaths > 3)
-                                    if (warrior.CurrentDamageLevel == JediKnight.DamageLevel.Critical)
-                                    {
-                                        warrior.FightLog.FightEvents.Add("Darth Vader hears a well-known voice from the void.. The Evil Emperor says: " + Palpatine.Dismay);
-                                        // Let Palpatine at the bastards! Critical condition or three times under the floorboards is enough for Darth.
-                                        Palpatine.DarkSideBoost(myJedis);
-                                        warrior.FightLog.FightEvents.Add("The Evil Emperor has shown his hand in dealing the Jedis a jaw-crusher, resetting a few damage levels.");
-                                    }
-                                    if (warrior.CurrentDamageLevel == JediKnight.DamageLevel.Hurting)
-                                    {
-                                        warrior.FightLog.FightEvents.Add("Darth Vader hears a well-known voice from the void.. The Evil Emperor says: " + Palpatine.Encouragement);
-                                    }
-                                }
-                                else // this happens if the Emperor points a bony finger at a dead DV
-                                {
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " unfortunately feels a bit out of it right now, and is rather actively knockin' on the Force's Door.");
-                                    // Darth is a mean ole bean, so we'll re-life him only with a lousy medicare allowance of 'Hurting'
-                                    warrior.Deceased = false;
-                                    warrior.CurrentDamageLevel = JediKnight.DamageLevel.Hurting;
-                                    warrior.FightLog.FightEvents.Add("Wow, that door-mojo worked... " + warrior.Name + " is in hurting condition, but still bouncing back for some mean ole revenge!");
-                                }
-
-                                // add player fight events to game log in sequence
-                                // gameLog.FightEvents.AddRange(warrior.fightLog.FightEvents);
-                                HtmlEncodeFightEvents(gameLog, warrior);
-                                break;
-
-                            case "Luke Skywalker":
-                                if (!warrior.Deceased)
-                                {
-                                    // Attack Dad!
-                                    warrior.AttackEnemy(warrior, DarthV);
-                                    if (myWarrior.DarkSide == true) { warrior.AttackEnemy(warrior, myWarrior); }
-
-                                    // if Luke Skywalker is is pressured with a health level of critical, let's bring Obi-Wan into the game. 
-                                    // NB: Only once, though.
-                                    if ((warrior.CurrentDamageLevel == JediKnight.DamageLevel.Critical)
-                                        && (!myJedis.Contains(ObiWan)))
-                                    {
-                                        ObiWan.SaveLuke(warrior);
-                                        myJedis.Add(ObiWan);
-                                        warrior.FightLog.FightEvents.Add("Obi-Wan helps Luke get healthy again and decides that now's the perfect time to join the fun! Woo-hoo!");
-                                    }
-                                    // If he's only hurting, encourage him a bit
-                                    if (warrior.CurrentDamageLevel == JediKnight.DamageLevel.Hurting)
-                                    {
-                                        // moral support from Obi-Wan for Luke
-                                        warrior.FightLog.FightEvents.Add("Luke hears a voice from just beyond his skull.. Obi-Wan says: " + ObiWan.Encouragement);
-                                    }
-                                }
-                                else
-                                {
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " unfortunately feels a bit out of it right now, and is feebly knockin' on the Force's Door, in a Lukey way.");
-                                    // give Luke an advantage when killed, by rescusiating him and by setting his health to medium
-                                    warrior.Deceased = false;
-                                    warrior.CurrentDamageLevel = JediKnight.DamageLevel.Challenged;
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " apparently has mucho clout with the Force's Door and is now very much alive and bouncing back for some swashbuckling revenge!");
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " faintly hears the whispering voice of " + ObiWan.Name + ": " + ObiWan.Encouragement);
-                                }
-
-                                // add player fight events to game log in sequence
-                                // gameLog.FightEvents.AddRange(warrior.fightLog.FightEvents);
-                                HtmlEncodeFightEvents(gameLog, warrior);
-                                break;
-
-                            case "Obi-Wan Kenobi":
-                                if (!warrior.Deceased)
-                                {
-                                    // Attack Darth!
-                                    warrior.AttackEnemy(warrior, DarthV);
-                                    // Attack the player object, if he's in a Sith kind of way
-                                    if (myWarrior.DarkSide == true) { warrior.AttackEnemy(warrior, myWarrior); }
-                                }
-                                else
-                                {
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " unfortunately feels a bit out of it right now, and is feebly knockin' on the Force's Door, in an Obi-Wan kind of way.");
-                                    // give Obi-Wan an advantage when killed, by rescusiating him and by setting his health to medium
-                                    warrior.Deceased = false;
-                                    warrior.CurrentDamageLevel = JediKnight.DamageLevel.Challenged;
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " apparently has mucho clout with the Force's Door and is now very much alive and bouncing back for some swashbuckling revenge!");
-                                }
-                                // add player fight events to game log in sequence
-                                // gameLog.FightEvents.AddRange(warrior.fightLog.FightEvents);
-                                HtmlEncodeFightEvents(gameLog, warrior);
-                                break;
-
-                            // default case will be our guy on the dance floor wanting a serious piece of the action
-                            default:
-                                if (!warrior.Deceased)
-                                {
-                                    if (warrior.DarkSide == false) { warrior.AttackEnemy(warrior, DarthV); }
-                                    if (warrior.DarkSide == true) { warrior.AttackEnemy(warrior, LukeS); }
-                                }
-                                else
-                                {
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " unfortunately feels a bit out of it right now, and is knockin' on the Force's Door.");
-                                    warrior.Deceased = false;
-                                    warrior.CurrentDamageLevel = JediKnight.DamageLevel.Challenged;
-                                    warrior.FightLog.FightEvents.Add(warrior.Name + " apparently has some clout with the Force's Door and is now enlivened..bent on some challenged-level revenge!");
-                                }
-                                // add player fight events to game log in sequence
-                                // gameLog.FightEvents.AddRange(warrior.fightLog.FightEvents);
-                                HtmlEncodeFightEvents(gameLog, warrior);
-                                break;
-                        }
-                    }
-
-                    // Conclude fight at this point - check who's still vital, and do the victory roll
-                    //foreach (JediKnight warrior in myJedis)
-                    //{
-                    //    if (!warrior.Deceased)
-                    //    {
-                    //        gameLog.FightEvents.Add("Ho ho ho! " + warrior.Name + "'s still around, with a rude health of: " + warrior.currentDamageLevel
-                    //           + ". He has visited the earthworms " + warrior.NumOfDeaths + " times during the scrap.");
-
-                    //    }
-                    //    else
-                    //    {
-                    //        gameLog.FightEvents.Add("Awww, " + warrior.Name + " is sleeping with the fishes! He has visited the salty grave " 
-                    //            + warrior.NumOfDeaths + " times during the scrap.");
-                    //    }
-                    //}
-
-                    // Set up a comparison based on how many deaths and what current health a Jedi Knight has to determine who won overall
-                    // Reference, http://stackoverflow.com/questions/230588/problem-sorting-lists-using-delegates:   
-                    // myList.Sort( delegate (MyType t1, MyType t2) { return (t1.ID.CompareTo(t2.ID)); } );
-
-                    myJedis.Sort(delegate (JediKnight j1, JediKnight j2) { return (j1.NumOfDeaths.CompareTo(j2.NumOfDeaths)); });
-
-                    // print out the guy who died the least amount of times
-                    gameLog.FightEvents.Add("<p><div><ul>");
-                    gameLog.FightEvents.Add("<li>" + "The honorable Jedi Knight " + myJedis[0].Name + " is the victor of the game, with " + myJedis[0].NumOfDeaths + " death experiences!" + "</li>");
-
-                    List<JediKnight> myHealthVictorList = myJedis;
-                    // take out the victor, his health ain't so interesting.
-                    myHealthVictorList.Remove(myJedis[0]);
-                    // re-order the list according to current health level, and then reverse it to print out a descending list of jedi dudes based on current health.
-                    myHealthVictorList.Sort(delegate (JediKnight j1, JediKnight j2) { return (j1.CurrentDamageLevel.CompareTo(j2.CurrentDamageLevel)); });
-                    myHealthVictorList.Reverse();
-                    foreach (JediKnight warrior in myHealthVictorList)
-                    {
-                        if (!warrior.Deceased)
-                        {
-                            string myIntro = ((bool)warrior.DarkSide ? "The Sith Lord " : "The honorable Jedi Knight ");
-                            gameLog.FightEvents.Add("<li>" + myIntro + warrior.Name + " has survived the last game iteration with a rude health of " + warrior.CurrentDamageLevel + "."
-                                + ". He has visited the earthworms " + warrior.NumOfDeaths + " times during the scrap." + "</li>");
-                        }
-                        if (warrior.Deceased)
-                            gameLog.FightEvents.Add("<li>" + "Awww, " + warrior.Name + " is sleeping with the fishes, and so has a lousy current health certificate! He has visited the salty grave "
-                                + warrior.NumOfDeaths + " times during the scrap." + "</li>");
-                    }
-                    gameLog.FightEvents.Add("</ul></div></p>");
-
+                    
                     // Set up viewbag list of event strings
                     ViewBag.FightDescription = new List<string> { "<p><div class='lead'>" + "A long time ago in a galaxy far, far away...." + "</div></p>" };
 
